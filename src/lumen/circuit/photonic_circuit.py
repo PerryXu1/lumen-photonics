73d9a871d1651bf1af90e6bc099736567048752e
PerryXu1 <perryxu6626@gmail.com>
2025-12-15 01:43:29 -0500

    feat: change photonic circuit to connect componenets port-to-port

--- src/lumen/circuit/photonic_circuit.py
+++ src/lumen/circuit/photonic_circuit.py
@@ -1,34 +1,31 @@
 from collections.abc import Sequence, MutableSequence
+from typing import Optional
 from component import Component
+from exceptions import MissingComponentException
+from uuid import uuid4
 
-# TODO: Fix the name to Photonic Curcuit
-class Photonic_Circuit:
+class PhotonicCircuit:
     __slots__ = "_components", "input"
 
     def __init__(self):
-        self._components: MutableSequence[Component] = []
-        # TODO: should be Optional[Component] (remember to import as well)
+        self.id = uuid4()
+        self._components: Optional[Component] = []
         self.input: Component = None
 
-    # TODO: move this to the bottom of the class
-    @property
-    def components(self) -> Sequence[Component]:
-        return self._components
-
     def set_input(self, component: Component):
-        # TODO: create a custom exception
         if component not in self._components:
-            raise Exception("Component not in circuit")
+            raise MissingComponentException(component)
         self.input = component
 
     def add(self, component: Component) -> None:
         self._components.append(component)
 
     def connect(self, component1: Component, output_name: str | int, component2: Component, input_name: str | int) -> None:
-        # TODO: component1 not in self._components or component2 not in self._components:
-        if component1 or component2 not in self._components:
+        if component1 not in self._components or component2 not in self._components:
             raise Exception("One or both of the components are not in the circuit")
-        
-        component1.set_output(output_name, component2)
-        component2.set_input(input_name, component1)
+        component1.set_output(output_name, component2, input_name)
+        component2.set_input(input_name, component1, output_name)
     
+    @property
+    def components(self) -> Sequence[Component]:
+        return self._components
