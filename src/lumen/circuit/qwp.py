925fde8f75008b4bebfd224c723adef16a4ef86d
PerryXu1 <perryxu6626@gmail.com>
2025-12-23 21:31:56 -0500

    fix minor bugs

--- src/lumen/circuit/qwp.py
+++ src/lumen/circuit/qwp.py
@@ -6,7 +6,6 @@ class QWP(Component):
     _COMPONENT_NAME = "QWP"
 
     def __init__(self, fast_axis: Literal["vertical", "horizontal"]):
-        # TODO: format with spaces between operators
         if fast_axis == "vertical":
             s_matrix = np.exp((np.pi/4)*1j) * np.array([[1, 0],
                                                          [0, -1j]])
