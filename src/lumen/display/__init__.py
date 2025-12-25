a2d2ca381721e183483e7ce11b63af6cdb451627
PerryXu1 <perryxu6626@gmail.com>
2025-12-24 21:05:31 -0500

    feat: create init file to simplify imports

---
+++ src/lumen/display/__init__.py
@@ -0,0 +1,5 @@
+from .display import Display, DisplaySettings
+from .poincare import Poincare
+from .polarization_view_3d import PolarizationView3D
+
+__all__ = ['Display', 'DisplaySettings', 'Poincare', 'PolarizationView3D']
