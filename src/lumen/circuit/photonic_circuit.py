7e3422997fbe55112d43f745904939fc5e76303b
PerryXu1 <perryxu6626@gmail.com>
2025-12-27 15:05:16 -0500

    feat: change code to allow for multiple inputs and outputs

--- src/lumen/circuit/photonic_circuit.py
+++ src/lumen/circuit/photonic_circuit.py
@@ -1,7 +1,9 @@
-from collections.abc import Sequence
-from typing import Optional
+from collections.abc import MutableMapping, MutableSequence, Sequence, Optional
+
+from ..circuit.laser import Laser
+from ..models.port import Port
 from .component import Component
-from .exceptions import MissingComponentException
+from .exceptions import InvalidConnectionException, MissingComponentException, MissingPortException
 from uuid import uuid4
 
 class PhotonicCircuit:
@@ -10,26 +12,40 @@ class PhotonicCircuit:
     functional circuit.
     """
     
-    __slots__ = "_components", "input"
+    __slots__ = "_components", "inputs", "outputs"
 
     def __init__(self):
         """Constructor method
         """
         
         self.id = uuid4()
-        self._components: Optional[Component] = [] # list of componenets in the circuit
-        self.input: Component = None # the component that the laser light inputs to
+        self._components: Optional[Component] = [] # list of components in the circuit
+        self.inputs: MutableMapping[Port] = {} # the ports that the laser light inputs to
+        self.output: MutableSequence[Port] = [] # the ports at which the final state is desired
 
-    def set_input(self, component: Component) -> None:
-        """Sets the component that laser light source inputs to.
+    def set_input(self, port: Port, laser: Laser) -> None:
+        """Sets a port that laser light source inputs to.
         
-        :param component: The component that the laser light inputs to
-        :type component: Component
+        :param port: The port that the laser light inputs to
+        :type port: Port
+        :param laser: The laser used as input at that port
+        :type laser: Laser
         """
+        for component in self._components:
+            if port in component.inputs:
+                self.inputs[port] = laser
+        raise MissingPortException(component)
+    
+    def set_output(self, port: Port) -> None:
+        """Sets a port as an output, where the final state is desired.
         
-        if component not in self._components:
-            raise MissingComponentException(component)
-        self.input = component
+        :param port: The port that the laser light inputs to
+        :type port: Port
+        """
+        for component in self._components:
+            if port in component.inputs:
+                self.outputs.append(port)
+        raise MissingPortException(component)
 
     def add(self, component: Component) -> None:
         """Adds a component to the circuit.
@@ -55,7 +71,15 @@ class PhotonicCircuit:
         """
         
         if component1 not in self._components or component2 not in self._components:
-            raise Exception("One or both of the components are not in the circuit")
+            raise MissingComponentException("One or both of the components are not in the circuit")
+        
+        output_port = component1.search_by_output_alias(output_name)
+        input_port = component2.search_by_input_alias(input_name)
+        if output_port in self.outputs:
+            self.outputs.remove(output_port)
+        if input_port in self.inputs:
+            self.inputs.pop(input_port)
+            
         component1.set_output(output_name, component2, input_name)
         component2.set_input(input_name, component1, output_name)
     
