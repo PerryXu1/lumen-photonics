8a2f510478b1ce009d50185eddd7ca22af607de5
PerryXu1 <perryxu6626@gmail.com>
2025-12-30 14:00:47 -0500

    feat: update slots

--- src/lumen/circuit/components/qwp.py
+++ src/lumen/circuit/components/qwp.py
@@ -10,15 +10,23 @@ class QWP(Component):
         with the vertical or horizontal state of the light
     :type fast_axis: literal - either the string 'vertical' or the string 'horizontal'
     """
+    
+    __slots__ = ("id", "name", "_s_matrix", "_num_inputs", "_input_ports", "_input_port_aliases",
+                "_input_port_ids", "_num_outputs", "_output_ports", "_output_port_aliases",
+                "_output_port_ids", "_in_degree", "_out_degree")
     _COMPONENT_NAME = "QWP"
 
     def __init__(self, fast_axis: Literal["vertical", "horizontal"]):
         """Constructor method
         """
         if fast_axis == "vertical":
-            s_matrix = np.exp((np.pi/4)*1j) * np.array([[1, 0],
-                                                         [0, -1j]])
+            s_matrix =  np.array([[0, 0, 1j, 0],
+                                  [0, 0, 0, 1],
+                                  [1j, 0, 0, 0],
+                                  [0, 1, 0, 0]])
         elif fast_axis == "horizontal":
-            s_matrix = np.exp((np.pi/4)*1j) * np.array([[1, 0],
-                                                         [0, 1j]]) 
+            s_matrix =  np.array([[0, 0, 1, 0],
+                                  [0, 0, 0, 1j],
+                                  [1, 0, 0, 0],
+                                  [0, 1j, 0, 0]])
         super().__init__(self._COMPONENT_NAME, 1, 1, s_matrix)
